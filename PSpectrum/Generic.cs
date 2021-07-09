de5394088e613245991ec67ae734fc0a9aba0be3
malte-linke <malte.linke.ml@gmail.com>
2021-07-09 12:03:10 +0200

    removed PLib dependencies

---
+++ PSpectrum/Generic.cs
@@ -0,0 +1,25 @@
+﻿namespace PLib
+{
+    /// <summary>
+    /// Provides simplified types.
+    /// </summary>
+    public static class Generic
+    {
+        /// <summary>
+        /// Simplified version of the System.Timers.Timer-Type
+        /// </summary>
+        public class EasyTimer : System.Timers.Timer
+        {
+            /// <summary>
+            /// Simplified version of the System.Timers.Timer-Type
+            /// </summary>
+            public EasyTimer(double delay)
+            {
+                this.Interval = delay;
+                this.Enabled = true;
+                this.AutoReset = true;
+                this.Start();
+            }
+        }
+    }
+}
