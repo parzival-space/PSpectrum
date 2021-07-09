92190c9f5da449da906f67461ee9f1bbe018c833
malte-linke <malte.linke.ml@gmail.com>
2021-07-09 11:23:19 +0200

    init

---
+++ PSpectrum/Program.cs
@@ -0,0 +1,166 @@
+﻿using NAudio.CoreAudioApi;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+using static PLib.Generic;
+
+namespace PSpectrum
+{
+    #region IGNORED
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "PSpectrum by Malte Linke";
+
+            // extract required files
+            if (!File.Exists("bass.dll")) File.WriteAllBytes("bass.dll", Properties.Resources.bass);
+            if (!File.Exists("basswasapi.dll")) File.WriteAllBytes("basswasapi.dll", Properties.Resources.basswasapi);
+
+            var analyser = new PAnalyser();
+            analyser.DataReady += (s, d) =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(d));
+            };
+            analyser.Start();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+    }
+
+    #endregion IGNORED
+
+    internal class PAnalyser
+    {
+        private WASAPIPROC _process;
+        private EasyTimer _t;
+        private float[] _buffer;
+
+        public delegate void OnDataReady(object sender, float[] data);
+
+        public event OnDataReady DataReady;
+
+        public PAnalyser()
+        {
+            // prepare bass api?
+            Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATETHREADS, false);
+            var result = Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            if (!result) throw new Exception("Init Error");
+
+            // prepare buffer
+            _buffer = new float[8192];
+
+            // initialize WASAPI-Process?
+            _process = new WASAPIPROC(Process);
+
+            // get my device index
+            var devIndex = 0;
+            var d = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
+            {
+                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                if (device.IsEnabled && device.IsLoopback && device.name == d.FriendlyName)
+                {
+                    if (Environment.GetCommandLineArgs().Contains("--print-device") || Environment.GetCommandLineArgs().Contains("-pd")) Console.WriteLine("Selected Device: {0}", device.name);
+                    devIndex = i;
+                }
+            }
+            result = BassWasapi.BASS_WASAPI_Init(devIndex, 0, 0, BASSWASAPIInit.BASS_WASAPI_BUFFER, 1f, 0.05f, _process, IntPtr.Zero);
+            if (!result)
+            {
+                var error = Bass.BASS_ErrorGetCode();
+                MessageBox.Show(error.ToString());
+            }
+
+            // init polling timer?
+            _t = new EasyTimer(20);
+            _t.Stop();
+            _t.Elapsed += (s, e) =>
+            {
+                // get raw data
+                int dataRaw = BassWasapi.BASS_WASAPI_GetData(_buffer, (int)BASSData.BASS_DATA_FFT8192);  //get ch.annel fft data
+                if (dataRaw < -1) return;
+
+                int shift = 15;
+
+                // convert raw data to 128 channels (64 left / 64 right)
+                /*float[] data = new float[128];
+                int range = (_buffer.Length / 3) / (data.Length / 2); // calculate the range of frequencys
+                for (int i = 0; i < (data.Length / 2) + shift; i++)
+                {
+                    if (i < shift) continue;
+                    float dataTemp = 0;
+                    for (int j = 0; j < range; j++)
+                    {
+                        dataTemp += (_buffer[j + (range * i)] < 0) ? 0 : _buffer[j + (range * i)];
+                    }
+
+                    // get channel audio level
+                    int level = BassWasapi.BASS_WASAPI_GetLevel();
+                    var audioLeft = Utils.LowWord32(level);
+                    var audioRight = Utils.HighWord32(level);
+
+                    int leftMult = ushort.MaxValue / ((audioLeft == 0) ? 1 : audioLeft);
+                    int rightMult = ushort.MaxValue / ((audioRight == 0) ? 1 : audioRight);
+
+
+                    // channel left
+                    data[i-shift] = dataTemp * leftMult;
+
+                    // channel right
+                    data[i- shift + 64] = dataTemp * rightMult;
+                }*/
+
+                // alternative channel selector
+                float[] data = new float[256];
+                for (int i = 0; i < 128 + shift; i++)
+                {
+                    if (i < shift) continue;
+                    float dataTemp = _buffer[i];
+
+                    // get channel audio level
+                    int level = BassWasapi.BASS_WASAPI_GetLevel();
+                    var audioLeft = Utils.LowWord32(level);
+                    var audioRight = Utils.HighWord32(level);
+
+                    int leftMult = ushort.MaxValue / ((audioLeft == 0) ? 1 : audioLeft);
+                    int rightMult = ushort.MaxValue / ((audioRight == 0) ? 1 : audioRight);
+
+
+                    // channel left
+                    data[i - shift] = dataTemp * leftMult;
+
+                    // channel right
+                    data[i - shift + 128] = dataTemp * rightMult;
+                }
+
+                // send to user
+                if (DataReady != null) DataReady(this, data);
+            };
+            _t.Interval = 25; // 40Hz
+        }
+
+        // WASAPI callback, required for continuous recording
+        private int Process(IntPtr buffer, int length, IntPtr user)
+        {
+            return length;
+        }
+
+        public void Start()
+        {
+            BassWasapi.BASS_WASAPI_Start();
+            _t.Start();
+        }
+
+        public void Stop()
+        {
+            BassWasapi.BASS_WASAPI_Stop(true);
+            _t.Stop();
+        }
+    }
+}
---
+++ PSpectrum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("PSpectrum")]
+[assembly: AssemblyDescription("This utility allows programms like Node.js to analyse the System audio. It acts like a 64-band equalizer and prints an array of the frequency to the console in an interval of 20ms.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PSpectrum")]
+[assembly: AssemblyCopyright("Copyright © Malte Linke 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("5e36a759-a124-4c26-8417-7fa7216f3569")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
